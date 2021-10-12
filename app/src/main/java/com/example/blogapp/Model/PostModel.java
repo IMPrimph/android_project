a089d5c4d624b8f0fcda4b48e0def36792bdd73b
IMPrimph <vishnugajulapalli123@gmail.com>
2021-10-12 15:41:58 +0530

    intial commit

---
+++ app/src/main/java/com/example/blogapp/Model/PostModel.java
@@ -0,0 +1,33 @@
+package com.example.blogapp.Model;
+
+public class PostModel {
+    String pImage, pTitle, pDescription;
+
+    public PostModel(){
+
+    }
+
+    public String getpImage() {
+        return pImage;
+    }
+
+    public void setpImage(String pImage) {
+        this.pImage = pImage;
+    }
+
+    public String getpTitle() {
+        return pTitle;
+    }
+
+    public void setpTitle(String pTitle) {
+        this.pTitle = pTitle;
+    }
+
+    public String getpDescription() {
+        return pDescription;
+    }
+
+    public void setpDescription(String pDescription) {
+        this.pDescription = pDescription;
+    }
+}
